2d6f87dad65a3c9d5c0e0d2b254b678053871324
Scott Offen <github@scottoffen.com>
2017-02-17 15:29:21 -0700

    Initial commit

---
+++ src/Grapevine/Interfaces/IHttpRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Grapevine.Interfaces
+{
+    public interface IHttpRequest
+    {
+        /*
+         * I want a common interface for both incoming requests (server) and outgoing requests (client) and methods for converting between the two.
+         * 
+         * Incoming Request: HttpListenerRequest
+         * https://msdn.microsoft.com/en-us/library/system.net.httplistenerrequest(v=vs.110).aspx
+         * 
+         * Outgoing Request: HttpWebRequest
+         * https://msdn.microsoft.com/en-us/library/system.net.httpwebrequest(v=vs.110).aspx
+         * 
+         */
+    }
+}
---
+++ src/Grapevine/Interfaces/IHttpResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Grapevine.Interfaces
+{
+    public interface IHttpResponse
+    {
+        /*
+         * I want a common interface for both outgoing responses (server) and incoming responses (client) and methods for converting between the two.
+         * 
+         * Outgoing Response: HttpListenerResponse
+         * https://msdn.microsoft.com/en-us/library/system.net.httplistenerresponse(v=vs.110).aspx
+         * 
+         * Incoming Response: HttpWebResponse
+         * https://msdn.microsoft.com/en-us/library/system.net.httpwebresponse(v=vs.110).aspx
+         * 
+         */
+    }
+}
